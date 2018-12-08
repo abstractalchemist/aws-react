d405c66e18998117f1f80fa85ccbd3ccfc37073b
Jason Hirata <jhirata@integrity-apps.com>
2018-12-08 09:13:25 -1000

    fixing login

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ import Amplify, { Auth } from 'aws-amplify'
 //import { withAuthenticator } from 'aws-amplify-react'
 
 Amplify.configure(aws_exports)
+const origin_url =  'https://d3eaxtdmx72vgn.cloudfront.net'
+
+const AmazonLoginButton = ({}) => {
+
+   const afterClick = evt => {
+      let options = { scope: 'profile' }
+      amazon.Login.authorize(options, 
+         function(response) {
+            if(response.error)
+               console.alert('Login Error')
+            else
+               amazon.Login.retrieveProfile(response.access_token, function(response) {
+                  alert(response)
+               })
+         })
+      evt.preventDefault()
+      return false;
+   }
+
+   return (
+      <a href="" id="LoginWithAmazon" onClick={afterClick}>
+          <img border="0" alt="Login with Amazon"
+                  src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+                  width="156" height="32" />
+      </a>
+   )
+}
 
 class Main extends React.Component {
 
@@ -14,7 +41,11 @@ class Main extends React.Component {
    }
 
    render() {
-      return (<div />)
+      return (
+      <div >
+      <AmazonLoginButton />
+      </div>
+         )
    }
 }
 
